ed251315b8aa8c093d2b5bf88237663661d2d8f4
kowshik <122955254+kowshikrg@users.noreply.github.com>
2024-06-01 01:08:23 -0700

    Add files via upload

---
+++ elms.sql
@@ -0,0 +1,217 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 22, 2021 at 05:58 AM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `elms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', '5c428d8875d2948607f3e3fe134d71b4', '2021-03-30 11:42:58');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbldepartments`
+--
+
+CREATE TABLE `tbldepartments` (
+  `id` int(11) NOT NULL,
+  `DepartmentName` varchar(150) DEFAULT NULL,
+  `DepartmentShortName` varchar(100) DEFAULT NULL,
+  `DepartmentCode` varchar(50) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbldepartments`
+--
+
+INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `DepartmentCode`, `CreationDate`) VALUES
+(1, 'Human Resource', 'HR', 'HR1', '2022-04-01 07:16:25'),
+(2, 'Development', 'DO', 'DO1', '2022-06-02 08:23:22'),
+(3, 'Test Team', 'QA', 'QA1', '2022-04-01 07:19:37'),
+(4, 'Operations', 'OP', 'OP1', '2022-04-02 21:28:56'),
+(5, 'Customer Support', 'CS', 'CS1', '2022-12-02 18:03:54');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblemployees`
+--
+
+CREATE TABLE `tblemployees` (
+  `id` int(11) NOT NULL,
+  `EmpId` varchar(100) NOT NULL,
+  `FirstName` varchar(150) DEFAULT NULL,
+  `LastName` varchar(150) DEFAULT NULL,
+  `EmailId` varchar(200) DEFAULT NULL,
+  `Password` varchar(180) DEFAULT NULL,
+  `Gender` varchar(100) DEFAULT NULL,
+  `Dob` varchar(100) DEFAULT NULL,
+  `Department` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `City` varchar(200) DEFAULT NULL,
+  `Country` varchar(150) DEFAULT NULL,
+  `Phonenumber` char(11) DEFAULT NULL,
+  `Status` int(1) DEFAULT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblemployees`
+--
+
+INSERT INTO `tblemployees` (`id`, `EmpId`, `FirstName`, `LastName`, `EmailId`, `Password`, `Gender`, `Dob`, `Department`, `Address`, `City`, `Country`, `Phonenumber`, `Status`, `RegDate`) VALUES
+(1, 'EMP001', 'Hamsa', 'Gowda', 'hamsa@gmail.com', 'hamsa@23', 'female', '23 December, 2002', 'Human Resource', 'Mysore', 'Mysore', 'India', '9857555655', 1, '2022-04-01 11:29:59'),
+(2, 'EMP002', 'Hriday', 'Gowda', 'hriday@gmail.com', 'hriday@20', 'Male', '20 April, 2022', 'Test Team', 'New Delhi', 'Delhi', 'India', '8587944255', 1, '2022-03-10 13:40:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblleaves`
+--
+
+CREATE TABLE `tblleaves` (
+  `id` int(11) NOT NULL,
+  `LeaveType` varchar(110) DEFAULT NULL,
+  `ToDate` varchar(120) DEFAULT NULL,
+  `FromDate` varchar(120) DEFAULT NULL,
+  `Description` mediumtext DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp(),
+  `AdminRemark` mediumtext DEFAULT NULL,
+  `AdminRemarkDate` varchar(120) DEFAULT NULL,
+  `Status` int(1) DEFAULT NULL,
+  `IsRead` int(1) DEFAULT NULL,
+  `empid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblleavetype`
+--
+
+CREATE TABLE `tblleavetype` (
+  `id` int(11) NOT NULL,
+  `LeaveType` varchar(200) DEFAULT NULL,
+  `Description` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblleavetype`
+--
+
+INSERT INTO `tblleavetype` (`id`, `LeaveType`, `Description`, `CreationDate`) VALUES
+(1, 'Casual Leave', 'Casual Leave ', '2021-04-01 12:07:56'),
+(2, 'Medical Leave test', 'Medical Leave  test', '202-04-06 13:16:09'),
+(3, 'Maternity Leave', 'Maternity Leave', '2022-04-06 13:16:38');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbldepartments`
+--
+ALTER TABLE `tbldepartments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblleaves`
+--
+ALTER TABLE `tblleaves`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `UserEmail` (`empid`);
+
+--
+-- Indexes for table `tblleavetype`
+--
+ALTER TABLE `tblleavetype`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbldepartments`
+--
+ALTER TABLE `tbldepartments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblleaves`
+--
+ALTER TABLE `tblleaves`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tblleavetype`
+--
+ALTER TABLE `tblleavetype`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
